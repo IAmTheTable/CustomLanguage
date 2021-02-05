1371f37371cba8faa15d1093ca1cc27788174ed3
TableFlipGod <tableflipgod352@gmail.com>
2021-02-05 20:58:35 +0000

    added more concatning

--- main.cs
+++ main.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.IO;
+using System.Text.RegularExpressions;
 public class Config{
   public static char EndOfLine = ';';
   public static char define = '=';
@@ -102,48 +103,91 @@ public static bool Recompile(string input){
           if(key.StartsWith(" ")){
             key = key.Substring(1);
           }
-          //Console.WriteLine("Key pt " + key + " " + isString(key));
-          if(isString(key)){
-            var tupleData = Tuple.Create(TokenType.Any,TokenType.String,Semi.Split(" ")[1].Split(Config.define)[0]);
+          if(tryGetValueFromData(key).Item1 == true || key.StartsWith("\"") && key.EndsWith("\"")){
+            //Console.WriteLine("Key pt " + key + " " + isString(key));
+            if(isString(key)){
+              var tupleData = Tuple.Create(TokenType.Any,TokenType.String,Semi.Split(" ")[1].Split(Config.define)[0]);
             //Console.WriteLine("key pt2 " + tupleData);
-            data.Add(tupleData,key);
-            break;
-          }
-          else{
+              data.Add(tupleData,key);
+              break;
+            }
+            else{
             //Console.WriteLine("xd" + Semi.Split("string")[1].Split(" ")[1].Split(" ")[0]);
-            var data2 = tryGetValueFromData(key);
+              var data2 = tryGetValueFromData(key);
             //Console.WriteLine("PreKek2" + key);
             //Console.WriteLine("KEK2" + data2);
-            var TupleData = Tuple.Create(TokenType.Any,TokenType.String,Semi.Split("string")[1].Split(" ")[1].Split(" ")[0]);
-            data.Add(TupleData, data2.Item2);
-            break;
+              var TupleData = Tuple.Create(TokenType.Any,TokenType.String,Semi.Split("string")[1].Split(" ")[1].Split(" ")[0]);
+              data.Add(TupleData, data2.Item2);
+              break;
+            }
           }
+          else{
+            throw new Exception("Exception thrown at line " + currentLine + " reason: string type invalid.");
+          }
+          
         case "int":
           var intkey = Semi.Split(Config.define)[1].Split(Config.EndOfLine)[0];
           if(intkey.StartsWith(" ")){
             intkey = intkey.Substring(1);
           }
-          if(isInt(Semi.Split(Config.define)[1].Split(Config.EndOfLine)[0]))
-          {
-            //Console.WriteLine("k"+key);
-            var TupleData = Tuple.Create(TokenType.Any, TokenType.Int, Semi.Split(" ")[1].Split(Config.define)[0]);
-            data.Add(TupleData,intkey);
-            break;
-          }
-          else{
-            //Console.WriteLine("heyo");
-            var TupleData = Tuple.Create(TokenType.Any, TokenType.Int, Semi.Split(" ")[1].Split(Config.define)[0]);
-            //Console.WriteLine("trying " + key);
-            try{
-              if(tryGetValueFromData(intkey).Item1 == true){
-                data.Add(TupleData, tryGetValueFromData(intkey).Item2);
+          List<int> intContents = new List<int>();
+          if(intkey.Split("+").Length > 1){
+            foreach(var keyVal in intkey.Split("+")){
+              var keyValFormat = keyVal;
+              if(keyValFormat.StartsWith(" ")){
+                keyValFormat = keyValFormat.Substring(1);
+              }
+              if(keyValFormat.EndsWith(" ")){
+                keyValFormat = keyValFormat.Remove(keyValFormat.Length - 1);
+              }
+              if(tryGetValueFromData(keyValFormat).Item1 == true){
+                intContents.Add(int.Parse(tryGetValueFromData(keyValFormat).Item2));
+                //Console.WriteLine("INt test" + int.Parse(tryGetValueFromData(keyValFormat).Item2) + "f");
+                //Console.WriteLine("tt" + Semi.Split("int ")[1].Split(Config.define)[0] + "tt");
               }
               else{
-                throw new Exception("Variable not found or initialized.");
+                intContents.Add(int.Parse(keyValFormat));
               }
             }
-            catch(Exception){}
+            int globalReturn = 0;
+            foreach(int key2 in intContents){
+              globalReturn = globalReturn + key2;
+            }
+            //Console.WriteLine("Got final int " + globalReturn + "f");
+            var varName = Semi.Split("int ")[1].Split(Config.define)[0];
+            if(varName.StartsWith(" ")){
+              varName = varName.Substring(1);
+            }
+            if(varName.EndsWith(" ")){
+              varName = varName.Remove(varName.Length - 1);
+            }
+            var TupleDataINt = Tuple.Create(TokenType.Any,TokenType.Int,varName);
+            data.Add(TupleDataINt, globalReturn.ToString());
             break;
+          }
+          else
+          {
+            if(isInt(Semi.Split(Config.define)[1].Split(Config.EndOfLine)[0])){
+              var TupleData = Tuple.Create(TokenType.Any, TokenType.Int, Semi.Split(" ")[1].Split(Config.define)[0]);
+              data.Add(TupleData,intkey);
+              break;
+            }
+            else{
+              var TupleData = Tuple.Create(TokenType.Any, TokenType.Int, Semi.Split(" ")[1].Split(Config.define)[0]);
+              //Console.WriteLine("trying " + key);
+              try{
+                if(tryGetValueFromData(intkey).Item1 == true){
+                  data.Add(TupleData, tryGetValueFromData(intkey).Item2);
+                }
+                else{
+                  throw new Exception("Variable not found or initialized.");
+                }
+              }
+              catch(Exception){}
+              break;
+            }
+            //Console.WriteLine("k"+key);
+
           }
         case "bool":
           var value = Semi.Split(Config.define)[1].Split(Config.EndOfLine)[0];
@@ -187,12 +231,69 @@ public static bool Recompile(string input){
       //Run the expressions
       switch(Semi.Split(Config.EndOfLine)[0].Split("(")[0]){
         case "Print":
+        List<string> variables = new List<string>();
           var data = Semi.Split(Config.EndOfLine)[0].Split("(")[1].Split(")")[0];
-          data.Remove(" ", "");
-          var TupleData = Tuple.Create(ExpressionType.Print,Semi.Split("Print")[1].Split(Config.EndOfLine)[0]);
-          var extraArgs = new string[1];
-          expressionData.Add(TupleData,extraArgs);
-          break;
+          if(data.Split("+").Length > 1){
+            foreach(var data2 in data.Split("+")){
+              var temp = data2;
+              //Console.WriteLine("PRECEHCK " + temp + " " + temp.Length);
+              if(temp.StartsWith(" ")){
+                temp = temp.Substring(1);
+                //Console.WriteLine("Starts with a space");
+              }
+              if(temp.EndsWith(" ")){
+                temp = temp.Remove(temp.Length - 1);
+                //Console.WriteLine("ends with space");
+              }
+              if(temp.StartsWith("\"") && temp.EndsWith("\"")){
+                variables.Add(temp.Split("\"")[1].Split("\"")[0]);
+              }
+              else if(temp.StartsWith("'") && temp.EndsWith("'")){
+                variables.Add(temp.Split("'")[1].Split("'")[0]);
+              }
+              else{
+                if(tryGetValueFromData(temp).Item1){
+                  variables.Add(tryGetValueFromData(temp).Item2);
+                }
+                else{
+                  throw new Exception("Variable " + temp + " not found or not defined at line " + currentLine);
+                }
+              }
+            }
+            List<string> finalData = new List<string>();
+            foreach(var x in variables){
+              var xVar = x;
+              if(xVar.StartsWith("\"")){
+                xVar = xVar.Substring(1);
+              }
+              if(xVar.EndsWith("\"")){
+                xVar = xVar.Remove(xVar.Length - 1);
+              }
+              finalData.Add(xVar);
+            }
+            var TupleData = Tuple.Create(ExpressionType.Print,Semi.Split("Print")[1].Split(Config.EndOfLine)[0]);
+            var extraArgs = finalData.ToArray();
+            expressionData.Add(TupleData,extraArgs);
+            break;
+          }
+          else{
+            if(tryGetValueFromData(data).Item1 == true){
+              var TupleData = Tuple.Create(ExpressionType.Print,Semi.Split("Print")[1].Split(Config.EndOfLine)[0]);
+              var extraArgs = new string[1];
+              //Console.WriteLine("data " + data);
+              extraArgs[0] = tryGetValueFromData(data).Item2.Split("\"")[1].Split("\"")[0];
+              expressionData.Add(TupleData,extraArgs);
+              break;
+            }
+            else{
+              var TupleData = Tuple.Create(ExpressionType.Print,Semi.Split("Print")[1].Split(Config.EndOfLine)[0]);
+              var extraArgs = new string[1];
+              //Console.WriteLine("data " + data);
+              extraArgs[0] = data;
+              expressionData.Add(TupleData,extraArgs);
+              break;
+            }
+          }
         default:
           break;
       }
@@ -206,17 +307,19 @@ public static bool Recompile(string input){
 }
 public static bool ExecuteExpression(){
   foreach(var exp in expressionData.Keys){
-    //Console.WriteLine("Item val " + exp.Item2);
     switch(exp.Item1){
       case ExpressionType.Print:
-        if(exp.Item2.Contains("\"")){
-          Console.WriteLine(exp.Item2.Split("\"")[1].Split("\"")[0]);
+        string final = "";
+        foreach(var variable in expressionData[exp]){
+         final = final + variable;
         }
-        else if(!exp.Item2.Contains("\"") || !exp.Item2.Contains("'")){
-          if(tryGetValueFromData(exp.Item2.Split("(")[1].Split(")")[0].ToString()).Item1 == true){
-            Console.WriteLine(tryGetValueFromData(exp.Item2.Split("(")[1].Split(")")[0].ToString()).Item2);
-          }
+        if(final.StartsWith("\"")){
+          final = final.Substring(1);  
         }
+        if(final.EndsWith("\"")){
+          final = final.Remove(final.Length - 1);
+        }
+        Console.WriteLine(final);
         break;
       default: 
         break;
@@ -234,7 +337,10 @@ public static bool ExecuteExpression(){
     }
     foreach(var item in data.Keys){
       //Console.WriteLine(item + " - " + data[item]);
-    }    
+    }
+    foreach(var item in expressionData.Keys){
+      //Console.WriteLine(item + " - " + expressionData[item]);
+    } 
     ExecuteExpression();
   }
 }
